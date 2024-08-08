f8b7de361e91a3d2e489142fe49ad405eb3fd5aa
JoeyShapiro <joeyashapiro@gmail.com>
2024-08-07 21:03:25 -0500

    got the ui going and removed garbage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,20 @@
 <script>
 	import Header from './Header.svelte';
 	import '../app.css';
+	import Sidebar from './Sidebar.svelte';
 </script>
 
-<div class="app">
+<div class="app d-flex flex-row">
 	<Header />
+	<Sidebar />
 
-	<main>
+	<main class="d-flex flex-fill">
 		<slot />
 	</main>
 </div>
 
 <style>
 	.app {
-		display: flex;
-		flex-direction: column;
 		min-height: 100vh;
-	}
-
-	main {
-		flex: 1;
-		display: flex;
-		flex-direction: column;
-		padding: 1rem;
-		width: 100%;
-		max-width: 64rem;
-		margin: 0 auto;
-		box-sizing: border-box;
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,27 +16,36 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	{#if messages.length > 0}
-		<ul>
-			{#each messages as message}
-				<li>{message}</li>
-			{/each}
-		</ul>
-	{/if}
+<section class="d-flex flex-column flex-fill bg-gradient overflow-hidden">
+	<div id="chatbox" class="d-flex flex-column flex-fill overflow-auto">
+		{#if messages.length > 0}
+		{#each messages as message}
+			<div class="toast fade show m-2 w-50" role="alert" aria-live="assertive" aria-atomic="true">
+				<div class="toast-header">
+					<!-- <svg class="bd-placeholder-img rounded mr-2" width="20" height="20" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img"><rect fill="#007aff" width="100%" height="100%"></rect></svg> -->
+					<strong class="mr-auto m-1" style="">username</strong>
+					<small id="date" class="text-muted">time</small>
+				</div>
+				<div id="original" hidden>{message}</div>
+				<div class="toast-body text-body">
+					{message}
+				</div>
+			</div>
+		{/each}
+		{/if}
+	</div>
 
-	<form on:submit|preventDefault={handleSubmit}>
-		<input type="text" placeholder="Type something..." />
-		<input type="submit" value="Add message" />
-	</form>
+	<div id="chat" class="d-flex flex-column flex-fill bg-gradient">
+		<form class="input-group mb-3" on:submit|preventDefault={handleSubmit}>
+			<input type="text" style="width: auto;" class="form-control" aria-label="Sizing example input"
+				aria-describedby="inputGroup-sizing-default" placeholder="Message" id="send-text">
+			<div id="send-btn-listener">
+				<button style="width: auto; height: 100%;" class="btn btn-primary" type="button"
+					id="send-btn">Send</button>
+			</div>
+		</form>
+	</div>
 </section>
 
 <style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
 </style>
